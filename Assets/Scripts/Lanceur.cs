a642fffb9a60b2b6712d68696a1cc7eec3844123
Sammuel Rwota <e2289486@cmaisonneuve.qc.ca>
2026-09-02 10:08:22 -0400

    3ème commit : Initialisation du projet - Bootstrap

---
+++ Assets/Scripts/Lanceur.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/* Script pour charger la scène du jeu (bootstrap).
+- Création du singleton pour s'assurer qu'il n'y a qu'une seule instance de ce script
+- Chargement de la scène "JeuMulti" au démarrage
+*/
+public class Lanceur : MonoBehaviour
+{
+    public static Lanceur instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    void Start()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+
+}
